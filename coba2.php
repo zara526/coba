c0dc5901992b78c73a2ce6a8986bd553fad120a3
Zara Salsa Aulia <140480917+zara526@users.noreply.github.com>
2023-07-25 16:06:43 +0700

    tambah file coba2

---
+++ coba2.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Note</title>
+    </head>
+    <body style="background-color: #ebdde2;">
+        <h1 style="text-align: center">My Note It's my dream</h1>
+        <hr style="border-top: 10px dotted #C39EC7">
+        <marquee>My Note it's my dream</marquee>
+        <img src="img/mawar.jpg" height="500" width="2000" style="margin-botom:500px">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <ul>
+            <li>What is Lorem Ipsum</li>
+            <li>Origins and Discovery</li>
+            <li>Fuzzy Beginnings</li>
+            <br>
+        </ul>
+            <button> <a href="https://loremipsum.io/">More Info!</a></button>
+        <p>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
+        </p>
+        <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”</p>
+        <h2>About Lorem Ipsum</h2>
+        <table border="1">
+            <tr>
+                <th colspan="3" bgcolor="pink">Description Lorem Ipsum</th>
+            </tr>
+            <tr>
+                <td rowspan="4">
+                    <img src="img/ukhtie.jpg" alt="fotomu">
+                </td>
+            </tr>
+            <tr>
+                <td>font generator</td>
+                <td>A wise Instagram bio once said, “The happiness of your life depends on the quality of your thoughts.” Well, sometimes you can find a little extra happiness when you dress those thoughts up with a cool font.</td>
+            </tr>
+            <tr>
+                <td>Plugins</td>
+                <td>If you’re developing themes or just need to mockup your new page with some Lorem Ipsum dummy text, check out these easy to use WordPress plugins. Drop the files in your /plugins directory and active them via the Admin menu.</td>
+            </tr>
+            <tr>
+                <td>Generator</td>
+                <td>The classic latin passage that just never gets old, enjoy as much (or as little) lorem ipsum as you can handle with our easy to use filler text generator.</td>
+            </tr>
+        </table>
+        <form action="" method="post" style="margin-top: 30px">
+            <fieldset>
+                <legend style="font-size: 30px; font-weight: bold;">Contact me</legend>
+                <p>
+                    <label>Name : </label>
+                    <input type="text" name="name" placeholder="your name...">
+                </p>
+                <p>
+                    <label>Email : </label>
+                    <input type="email" name="subject" placeholder="your email...">
+                </p>
+                <p>
+                    <label>Subject : </label>
+                    <input type="text" name="name" placeholder="your message...">
+                </p>
+                <p>
+                <input type="submit" name="submit" value="Send">
+                </p>
+            </fieldset>
+        </form>
+        <footer style="background-color: #C39EC7;">
+        <h3>More</h3>
+        <ul>
+            <li><a href="https://www.petanikode.com/">Petani Kode</a></li>
+            <li><a href="https://www.itkoding.com/">IT Koding</a></li>
+        </ul>
+        </footer>
+    </body>
+</html>
