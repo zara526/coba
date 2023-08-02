de525c8ec0e30e9aec4a4d60800c9cee81f159fe
Zara Salsa Aulia <zaraaulia52@smp.belajar.id>
2023-08-02 11:08:54 +0700

    Tambah Looping dan Jumping PHP

--- examp1/belajar.php
+++ examp1/belajar.php
@@ -24,6 +24,7 @@ sayHello("Alwi");
 
 echo "</br>-------------------------- </br>";
 
+//array
 echo "Ini Array";
 $data = [1,2,3];
 
@@ -37,5 +38,35 @@ $makanan= [
 ];
 
 print_r($makanan);
+echo "</br>-------------------------- </br>";
+
+//Struktur kendali (Looping)
+echo "Ini Perulangan (Looping)</br>";
+for ($counter = 10; $counter > 0; $counter--){
+    echo "Halo Sofia $counter </br>";
+}
+
+echo "</br>-------------------------- </br>";
+
+$motor = ["Tril", "Ninja", "Astrea", "Ontel"];
+foreach($motor as $isi){
+    echo "Awi mau beli motor $isi </br>";
+}
+
+echo "</br>-------------------------- </br>";
+
+//Struktur kendali (Jumping)
+echo "Ini Perpindahan (Jumping)</br>";
+for($i = 1;$i<20;$i++){
+    if($i ==4){
+        echo "Looping berhenti pada i = $i</br>";
+        continue;
+    }else{
+        if($i == 13){
+            echo "Ini katanya $i angka keramat skip ae lah</br>";
+        }
+    }
+    echo "Nilai i : $i</br>";
+}
 
 ?>
